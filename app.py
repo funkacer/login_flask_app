67a33a8faa29f3decc19c402d59b2169cd307c14
Fun Kacer <funkacer@gmail.com>
2025-07-24 15:08:34 +0200

    Steamdeck working changes

--- app.py
+++ app.py
@@ -13,15 +13,15 @@ from flask_session import Session
 
 app = Flask(__name__)
 
-app.config("SESSION_PERMANENT") = False
-app.config("SESSION_TYPE") = "filesystem"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 @app.route("/")
 def index():
     return render_template("index.html", name=session.get("name"))
 
-@app.route("/login", methods=["GET", ""POST])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         session["name"] = request.form.get("name")
